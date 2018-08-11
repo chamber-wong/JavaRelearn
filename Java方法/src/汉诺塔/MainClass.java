c2b660d5c10b18955ca4fcb5bbbc0ba1f4b7dfac
Chamber wong <chamber_wang@163.com>
2018-08-11 10:17:34 +0800

    更新汉诺塔

--- Java方法/src/汉诺塔/MainClass.java
+++ Java方法/src/汉诺塔/MainClass.java
@@ -2,12 +2,18 @@ package 汉诺塔;
 
 public class MainClass {
 	public static void main(String[] args) {
-		
-	}
-	public static void doTowers(int topS , char from ,char buffer ,char to) {
-//		if() {
-//			
-//		}
-		doTowers(topS-1, from, buffer, to);
-	}
+        int nDisks = 3;
+        doTowers(nDisks, 'A', 'B', 'C');
+    }
+    public static void doTowers(int topN, char from, char inter, char to) {
+        if (topN == 1){
+            System.out.println("Disk 1 from "
+            + from + " to " + to);
+        }else {
+            doTowers(topN - 1, from, to, inter);
+            System.out.println("Disk "
+            + topN + " from " + from + " to " + to);
+            doTowers(topN - 1, inter, from, to);
+        }
+    }
 }
